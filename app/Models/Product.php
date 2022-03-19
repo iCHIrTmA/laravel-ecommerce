7db10607fdbd9242a35b39fa0356eccfd3773810
janroeNabepero <janroe@nabepero.co.jp>
2022-03-19 09:10:32 +0800

    add product images w spatie media library

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,10 +6,15 @@ use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
@@ -21,5 +26,11 @@ class Product extends Model
     public function variations(): HasMany
     {
         return $this->hasMany(Variation::class);
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
     }
 }
