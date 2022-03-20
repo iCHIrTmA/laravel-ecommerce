1ee0acd29363c982164f0fac14177b19eef3c964
janroeNabepero <janroe@nabepero.co.jp>
2022-03-20 12:38:54 +0800

    show the cart in the navigation

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 
 class Cart extends Model
@@ -23,5 +24,10 @@ class Cart extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function variations(): BelongsToMany
+    {
+        return $this->belongsToMany(Variation::class);
     }
 }
