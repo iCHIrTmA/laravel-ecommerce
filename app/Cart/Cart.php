966ab96ba48193c5add9d456d41e3fb3c874c0e0
janroeNabepero <janroe@nabepero.co.jp>
2022-03-21 06:22:07 +0800

    cache the cart instance

--- app/Cart/Cart.php
+++ app/Cart/Cart.php
@@ -9,6 +9,8 @@ use Illuminate\Session\SessionManager;
 
 class Cart implements CartInterface
 {
+    protected $instance;
+
     public function __construct(protected SessionManager $session) { }
 
     public function exists(): bool
@@ -41,6 +43,10 @@ class Cart implements CartInterface
 
     protected function instance()
     {
-        return ModelsCart::whereUuid($this->session->get(config('cart.session.key')))->first();
+        if ($this->instance) {
+            return $this->instance;
+        }
+
+        return $this->instance = ModelsCart::whereUuid($this->session->get(config('cart.session.key')))->first();
     }
 }
