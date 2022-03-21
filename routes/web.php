068e76178952b1c89f241ce3e6f0804955506236
janroeNabepero <janroe@nabepero.co.jp>
2022-03-21 18:36:44 +0800

    show the users cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartIndexController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductShowController;
@@ -16,6 +17,7 @@ use App\Http\Controllers\ProductShowController;
 */
 
 Route::get('/', HomeController::class);
+Route::get('/cart', CartIndexController::class);
 Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
