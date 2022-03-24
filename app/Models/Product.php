45f6ff09d451855f173864eee7d1996967b1b91b
janroeNabepero <janroe@nabepero.co.jp>
2022-03-24 13:04:11 +0800

    index product variations for filtering

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -50,12 +50,17 @@ class Product extends Model implements HasMedia
 
     public function toSearchableArray()
     {
-        return [
+        return array_merge([
             'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
             'price' => $this->price,
             'category_ids' => $this->categories->pluck('id')->toArray(),
-        ];
+        ], $this->variations->groupBy('type')
+            ->mapWithKeys(fn ($variation, $key) => [
+                $key => $variation->pluck('title')  
+            ])
+            ->toArray()
+        );
     }
 }
