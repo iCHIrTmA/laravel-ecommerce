a21b7083d458c349221f95cfd2b132ae72d32f75
janroeNabepero <janroe@nabepero.co.jp>
2022-03-27 08:31:30 +0800

    handle products that are not live

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\LiveScope;
 use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -20,6 +21,11 @@ class Product extends Model implements HasMedia
     use Searchable;
 
     protected $guarded = [];
+
+    public static function booted()
+    {
+        static::addGlobalScope(new LiveScope());   
+    }
 
     public function formattedPrice()
     {
