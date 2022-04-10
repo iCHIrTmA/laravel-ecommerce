845ffcd389f735e69b218e8c51087a19f8f31c51
janroeNabepero <janroe@nabepero.co.jp>
2022-04-10 10:38:20 +0800

    redirect if the cart is empty

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\ProductShowController;
 */
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/cart', CartIndexController::class);
+Route::get('/cart', CartIndexController::class)->name('cart');
 Route::get('/checkout', CheckoutIndexController::class);
 Route::get('/categories/{category:slug}', CategoryShowController::class);
 Route::get('/products/{product:slug}', ProductShowController::class);
